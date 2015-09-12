0fa5f5283960a78391894fb722d03c6752b93350
Carlos Gochez <cgotcha@gmail.com>
2015-09-12 10:37:37 -0600

    Update EmpleadoTest

--- src/EmpleadoTest.java
+++ src/EmpleadoTest.java
@@ -20,13 +20,13 @@ public class EmpleadoTest {
         Empleado e2=new Empleado(1,"Delia");
         Empleado e3=new Empleado(2,"Martin",15000);
         
-        //e1 = new Empleado(3,"Jose");
+        e1.setNombre("Mr Bean");
+        e2.setEdad(21);
+        e3.setSalario(25000);
         
-        System.out.println("Codigo: "+e1.codigo+"-"+e1.nombre+"-Lps."+
-                e1.salario+"-"+e1.depto.nombre);
-        System.out.println("Codigo: "+e2.codigo+"-"+e2.nombre+"-Lps."+
-                e2.salario+"-"+e2.depto.nombre);
-        System.out.println("Codigo: "+e3.codigo+"-"+e3.nombre+"-Lps."+
-                e3.salario+"-"+e3.depto.nombre);
+        System.out.println("Codigo: "+e1.getCodigo()+"-"+e1.getNombre()+"-Lps."+
+                e1.getSalario()+"-"+e1.getDepartamento().getNombre());
+        e2.print();
+        e3.print();
     }
 }
